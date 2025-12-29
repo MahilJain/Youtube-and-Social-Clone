c8bbbf1fe71e3717b80094748c01cc1900dfc36a
MahilJain <Mahiljee001@gmail.com>
2025-12-29 16:17:54 +0530

    util codes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+//const asyncHandler = () => {}
+//const asyncHandler = (fun) => () => {}
+//const asyncHandler = () => async () => {}
+
+    //2nd-Method
+// const asyncHandler = (fn) => async (req, res ,next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             sucees: false,
+//             message: err.message
+//         })
+//     }
+// }
