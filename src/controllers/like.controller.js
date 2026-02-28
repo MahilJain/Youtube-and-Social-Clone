97052f1f7a155e0138556255cd111e8f3d0fb1a3
MahilJain <Mahiljee001@gmail.com>
2026-02-28 12:59:05 +0530

    implement toggle like functionality for comments and tweets; enhance get liked videos logic

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -52,18 +52,68 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
         return res.status(200).json(
             new ApiResponse(200, null, "Like removed successfully")
         )
+    } else {
+        const like = await Like.create({
+            comment: commentId,
+            likedBy: userId
+        })
+        return res.status(201).json(
+            new ApiResponse(201, like, "Comment liked successfully")
+        )
     }
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
-    //TODO: toggle like on tweet
+    
+    //steps for toggling like on tweet:
+    //1. validate tweetId
+    //2. check if like already exists for the tweet and user
+    //3. if like exists, remove it and return response
+    //4. if like does not exist, create it and return response
+
+    if(!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+    const userId = req.user?._id;
+    const likeExists = await Like.findOne({
+        tweet: tweetId,
+        user:userId
+    })
+    if(likeExists) {
+        await Like.findByIdAndDelete(likeExists._id)
+        return res.status(200).json(
+            new ApiResponse(200, null, "Like removed successfully")
+        )
+    } else {
+        const like = await Like.create({
+            tweet: tweetId,
+            likedBy: userId
+        })
+        return res.status(201).json(
+            new ApiResponse(201, like, "Tweet liked successfully")
+        )
+    }
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    //steps to get all liked videos:
+    //1. get user ID from request
+    //2. find all likes for the user where video field is not null
+    //3. populate the video field to get video details
+    //4. return the list of liked videos in response
+    
+    const userId = req.user?._id
+    const likedVideos = await Like.find({
+        likedBy: userId,
+        video: { $ne: null }
+    }).populate("video")
+    return res.status(200).json(
+        new ApiResponse(200, likedVideos, "Liked videos retrieved successfully")
+    )
+
 })
 
 export {
